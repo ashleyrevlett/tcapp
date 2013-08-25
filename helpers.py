31b39223c533bbe97f7c94bda73bea2a2e47700b
ashley revlett <ashleyrevlett@gmail.com>
2013-08-24 23:46:31 -0400

    added more debugging readouts

--- helpers.py
+++ helpers.py
@@ -7,15 +7,6 @@ from config import *
 Helper functions 
 """
 
-RED = (255,0,0)
-GREEN = (0,255,0)
-BLUE = (0,0,255)
-DARKBLUE = (0,0,128)
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-GRAY = (100,100,100)
-PINK = (255,200,200)
-
 
 def random_color():
 	r = random.randrange(0, 255, 1)
--- tcapp.py
+++ tcapp.py
@@ -34,7 +34,7 @@ class TCApp:
 		self.tile_size = TILE_SIZE
 		self.map_width = map_width
 		self.map_height = map_height
-		self.base_font = pygame.font.SysFont("helvetica", 8)
+		self.base_font = pygame.font.SysFont("helvetica", 7)
 		self.menu_font = pygame.font.SysFont("monospace", 14)
 
 		# init screen
@@ -52,6 +52,9 @@ class TCApp:
 			for i in xrange(SMOOTH_PASSES):
 				self.evolve_state()
 
+		# refine the granularity
+		self.refine_grid()
+
 		#wait for events in loop
 		self.loop()
 
@@ -66,9 +69,9 @@ class TCApp:
 			if event.type == pygame.QUIT:
 				running = 0
 			
-			if keys[pygame.K_RETURN]:
-				self.evolve_state()
-				time.sleep (50.0 / 1000.0)
+			# if keys[pygame.K_RETURN]:
+			# 	self.evolve_state()
+			# 	time.sleep (50.0 / 1000.0)
 				
 			if keys[pygame.K_ESCAPE]:
 				self.draw_grid()
@@ -101,39 +104,80 @@ class TCApp:
 	def draw_grid(self):
 
 		# init the grid
-		self.cols = int((self.map_width-20)/self.tile_size)
-		self.rows = int((self.map_height-40)/self.tile_size)
+		self.cols = int((self.map_width)/self.tile_size)
+		self.rows = int((self.map_height)/self.tile_size)
 		self.tiles = [[0 for x in xrange(0, self.rows, 1)] for x in xrange(0, self.cols, 1)] 
-
+		random.seed()
 		for i in xrange(0, self.cols, 1):
 			for j in xrange(0, self.rows, 1):
-				random.seed()
-				rnd_key = random.randint(0,100)
-				if rnd_key > 50: 
-					self.tiles[i][j] = 1
+
+				#if on border of world, make water
+				if i == 0 or i == self.cols-1 or j == 0 or j == self.rows-1:
+					self.tiles[i][j] = 0				
 				else:
-					self.tiles[i][j] = 0
-			
+					rnd_key = random.randint(0,100)
+					if rnd_key > 50: 
+						self.tiles[i][j] = 1
+					else:
+						self.tiles[i][j] = 0
+				
 		# now that random grid data is set up, draw the recs
 		self.draw_current_state()
 
 
+
+	def refine_grid(self):
+
+		# re-init the grid
+		self.tile_size = TILE_SIZE_FINE
+		self.cols = int((self.map_width)/self.tile_size)
+		self.rows = int((self.map_height)/self.tile_size)
+		new_tiles = [[0 for x in xrange(0, self.rows, 1)] for x in xrange(0, self.cols, 1)] 
+		random.seed()
+		for i in xrange(0, self.cols, 1):
+			for j in xrange(0, self.rows, 1):
+				parent_i = clamp(i/5,0,self.map_width)
+				parent_j = clamp(j/5,0,self.map_height)
+				
+				# if on border of world, make water
+				if i == 0 or i == self.cols-1 or j == 0 or j == self.rows-1:
+					new_tiles[i][j] = 0				
+				else:
+					# chance of being same as parent is 50%
+					rnd_key = random.randint(0,100)
+					new_tiles[i][j] = self.tiles[parent_i][parent_j]
+					# if rnd_key > 80: 
+					# 	new_tiles[i][j] = self.tiles[parent_i][parent_j]
+					# else:
+					# 	new_tiles[i][j] = rnd_key
+				print 'i:' + str(i) + ' j:' + str(j) + ' parent_i: ' + str(parent_i) + ' parent_j: ' + str(parent_j) + ' parent z: ' + str(self.tiles[parent_i][parent_j]) + ' child z: ' + str(new_tiles[i][j])
+		self.tiles = new_tiles
+		# now that random grid data is set up, draw the recs
+		self.draw_current_state()
+
+
+
+
+
 	def draw_current_state(self):
 		# draw each tile and its label
-		offset_x = 10
+		offset_x = 0
 		offset_y = 30
 		for i in xrange(0, self.cols, 1):
 			for j in xrange(0, self.rows, 1):
 				# draw rect outline
-				pygame.draw.rect(self.screen, (self.tiles[i][j]*255,self.tiles[i][j]*255,self.tiles[i][j]*255), ((i*self.tile_size)+offset_x, (j*self.tile_size)+offset_y, TILE_SIZE, TILE_SIZE), 0)									
-				pygame.draw.rect(self.screen, GRAY, ((i*self.tile_size)+offset_x, (j*self.tile_size)+offset_y, TILE_SIZE, TILE_SIZE), 1)									
-				# draw label
-				# draw title screen		
-				text_surface = TILE_SIZE			
-				label_text = str(i) + ', ' + str(j)
-				label = self.base_font.render(label_text, 1, PINK)
-				label_rect = pygame.Rect( (TILE_SIZE*i+offset_x,TILE_SIZE*j+offset_y), (TILE_SIZE+10,TILE_SIZE+10))
-				self.screen.blit(label,label_rect)
+				color_val = clamp(self.tiles[i][j]*255, 0, 255)
+				pygame.draw.rect(self.screen, (color_val,color_val,color_val), ((i*self.tile_size)+offset_x, (j*self.tile_size)+offset_y, self.tile_size, self.tile_size), 0)									
+				
+				if TESTING == True and self.tile_size != TILE_SIZE_FINE:
+					#draw border
+					pygame.draw.rect(self.screen, GRAY, ((i*self.tile_size)+offset_x, (j*self.tile_size)+offset_y, self.tile_size, self.tile_size), 1)									
+					# draw label					
+					text_surface = self.tile_size			
+					label_text = str(i) + ', ' + str(j)
+					label = self.base_font.render(label_text, 1, PINK)
+					label_rect = pygame.Rect( (self.tile_size*i+offset_x,self.tile_size*j+offset_y), (self.tile_size+10,self.tile_size+10))
+					self.screen.blit(label,label_rect)
 
 
 		pygame.display.flip()
@@ -146,16 +190,18 @@ class TCApp:
 
 				# if i == 0 or i == self.rows or j == 0 or j == self.cols: 
 				# 	new_tiles[i][j] = 0
-
-				state = self.tiles[i][j]
-				neighbors = self.get_neighbors(i, j)
-				
-				if state == 1 and sum(neighbors) >= 3:
-					new_tiles[i][j] = 1
-				elif state == 0 and sum(neighbors) >= 4:
-					new_tiles[i][j] = 1
+				if i == 0 or i == self.cols-1 or j == 0 or j == self.rows-1:
+					self.tiles[i][j] = 0
 				else:
-					new_tiles[i][j] = 0
+					state = self.tiles[i][j]
+					neighbors = self.get_neighbors(i, j)
+					
+					if state == 1 and sum(neighbors) >= 3:
+						new_tiles[i][j] = 1
+					elif state == 0 and sum(neighbors) >= 4:
+						new_tiles[i][j] = 1
+					else:
+						new_tiles[i][j] = 0
 
 		self.tiles = new_tiles
 		self.draw_current_state()
